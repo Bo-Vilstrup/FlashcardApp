928fb2e0c2b8845d7943e6de9850aa008ecc322c
Bo Vilstrup Mortensen <bo.vilstrup@gmail.com>
2016-12-19 01:26:03 +0100

    added and finised createflashcars in the frontend

--- public/views/createFlashcards/createFlashcardsController.js
+++ public/views/createFlashcards/createFlashcardsController.js
@@ -2,8 +2,6 @@
  * Created by bo on 12/18/16.
  */
 'use strict';
-
-//angular.module('myApp.view1', ['ngRoute', 'ngAnimate', 'ui.bootstrap'])
 angular.module('app.createFlashcardsModule', ['ngRoute'])
 
     .config(['$routeProvider', function ($routeProvider) {
@@ -18,6 +16,75 @@ angular.module('app.createFlashcardsModule', ['ngRoute'])
     .controller('createFlashcardsController', function ($http, $scope) {
 
 
+        function arrayRotate(arr, reverse){
+            if(reverse)
+                arr.unshift(arr.pop());
+            else
+                arr.push(arr.shift());
+            return arr
+        }
+        
+        var RandomRotate = function (array) {
+            var randomNumber = Math.round(Math.random() * 33); // number between 0..4
 
+            while(0 < --randomNumber) {
+                arrayRotate(array, true);
+            }
+            return array;
+        };
+        
 
+        $scope.submit = function () {
+
+            var answers = [
+                {
+                    "answer": $scope.correctAnswer,
+                    "isCorrect": true
+                },
+                {
+                    "answer": $scope.wrongAnswer1,
+                    "isCorrect": false
+                },
+                {
+                    "answer": $scope.wrongAnswer2,
+                    "isCorrect": false
+                },
+                {
+                    "answer": $scope.wrongAnswer3,
+                    "isCorrect": false
+                }
+            ];
+            
+            var RotatedAnswers = RandomRotate(answers);
+
+            var flashcard = {
+                "category": $scope.category,
+                "question": $scope.question,
+                "answers" : RotatedAnswers
+            };
+            
+            var req = {
+                method: 'POST',
+                url: '/api/card',
+                headers: {'Content-Type': 'application/json'},
+                data: flashcard
+            };
+            
+            $http(req).then(function successCallback(response) {
+
+                // $scope.category = "";
+                // $scope.question = "";
+
+                $scope.correctAnswer = "";
+                $scope.wrongAnswer1 = "";
+                $scope.wrongAnswer2 = "";
+                $scope.wrongAnswer3 = "";
+
+            }, function errorCallback(response) {
+               
+
+            }); // End of get()
+        }; // End of submit
     }); // End of Controller
+
+
